e9239baa2373b5e946080a7ec0d1aef334ca832a
henok <henokayele379@gmail.com>
2022-06-06 16:36:07 +0300

    ddsvc

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,14 @@ int main(void)
 
            srand(time(0));
            n = rand() - RAND_MAX / 2;
+
             if (n == 0)
             {
-                 printf("%i is zero\n", n);
+            printf("%i is zero\n", n);
             }
             else if (n < 0)
             {
-                 printf("%i is negative\n", n);
+            printf("%i is negative\n", n);
             }
             else 
             {
