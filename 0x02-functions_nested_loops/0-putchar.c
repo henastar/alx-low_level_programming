94f4eb0a4c23cdf102b184c260f0581d2ab6fd2c
henok <henokayele379@gmail.com>
2022-06-07 17:27:06 +0300

    dsds

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
-#include "main.h"
 #include<stdio.h>
-void print_times_table(int n);
-int main(void)
+#include "main.h"
+/**
+ *_putchar - writes the character c to to stdout
+ *@c: The character to print
+ *
+ *Return:on sucess 1.
+ * on error. -1 is returned. and errno is set appropriately.
+ */
+int _putchar(char c)
 {
  putchar("_putchar\n");
 }
