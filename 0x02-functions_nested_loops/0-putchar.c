81715610a75513bdb59296b6d39a499af98563e1
henok <henokayele379@gmail.com>
2022-06-08 10:32:27 +0300

    dcd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include "main.h"
+
+
 /**
- *  * main - Entry point
- *   *
- *     * Return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-write(1, "_putchar", 57);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+-putchar('\n');
+
 return (0);
 }
