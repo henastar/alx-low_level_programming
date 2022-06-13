38dcb8611813a8dfe3c44dd7f26d0f74a2f2e2ab
henok <henokayele379@gmail.com>
2022-06-13 16:24:22 +0300

    ddddd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strien - length of a string
+ * _strlen - length of a string
  * @s: A pointer to an int that will be changed/updated
  *
  * Return: void that means our answer is correct
  */
 
-int _strien(char *s)
+int _strlen(char *s)
 {
 int u;
 
